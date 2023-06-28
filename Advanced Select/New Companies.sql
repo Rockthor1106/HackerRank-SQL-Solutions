eeb7b595eed47fded2b0a6940b134341b9d734c0
Rockthor <jan.carvajal1@u.icesi.edu.co>
2023-06-28 16:33:26 -0500

    Solved-Draw The Triangle 1 The last commit was an error

--- Advanced Select/New Companies.sql
+++ Advanced Select/New Companies.sql
@@ -1,5 +1,1 @@
 --by @Rocthor1106
-SET @n := 21;
-
-SELECT REPEAT('* ', @n := @n - 1)--This decrements @n by one each time it is called, so @n starts with 20 value
-FROM information_schema.tables;
---
+++ Alternative Queries/Draw The Triangle 1.sql
@@ -0,0 +1,5 @@
+--by @Rockthor 
+SET @n := 21;
+
+SELECT REPEAT('* ', @n := @n - 1)--This decrements @n by one each time it is called, so @n starts with 20 value
+FROM information_schema.tables;
